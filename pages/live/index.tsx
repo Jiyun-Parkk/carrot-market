4b41be416ede58dce964745e96e7648f2f6a07c7
박지윤 <jyun1937@naver.com>
2023-02-20 13:14:51 +0900

    Style: live 컴포넌트에  fullbutton componenent 적용 Style: live 컴포넌트에 layout 컴포넌트 적용

--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -2,20 +2,32 @@ import { FloatButton, Layout } from 'components';
 import { NextPage } from 'next';
 import { BsCameraVideo } from 'react-icons/bs';
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const Streams: NextPage = () => {
+  const router = useRouter();
+  const onClickStream = (i: number) => {
+    router.push(`/live/${i}`);
+  };
+  const onClickFloatButton = () => {
+    router.push('/live/create');
+  };
   return (
     <Layout title='라이브' hasTabBar>
       <div className='divide-y-2 py-10'>
         {[1, 2, 3, 4, 5].map((_, i) => (
-          <div key={i} className='px-4 pt-4'>
+          <div
+            onClick={() => onClickStream(i)}
+            key={i}
+            className='cursor-pointer px-4 pt-4'
+          >
             <div className='aspect-video w-full rounded-md bg-slate-300 shadow-sm' />
             <h3 className='mt-2 text-lg text-gray-700'>
               Let&apos;s try potatos
             </h3>
           </div>
         ))}
-        <FloatButton onClick={() => {}}>
+        <FloatButton onClick={onClickFloatButton}>
           <BsCameraVideo />
         </FloatButton>
       </div>
