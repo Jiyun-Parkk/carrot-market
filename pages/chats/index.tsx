7dd7889173bfe615635ce4a53682aefe0147e06b
박지윤 <jyun1937@naver.com>
2023-02-20 09:25:49 +0900

    Feat: Layout 컴포넌트 추가

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,19 +1,22 @@
+import { Layout } from 'components';
 import { NextPage } from 'next';
 import React from 'react';
 
 const Chats: NextPage = () => {
   return (
-    <div className='divide-y-[1px] py-10'>
-      <div className='flex cursor-pointer items-center space-x-3 px-4 py-3'>
-        <div className='h-12 w-12 rounded-full bg-slate-300' />
-        <div>
-          <p className='text-gry-700 text-sm'>Steve Jebs</p>
-          <p className='text-sm text-gray-500'>
-            See you tomorrow in the corner at 2pm!
-          </p>
+    <Layout title='채팅' hasTabBar>
+      <div className='divide-y-[1px] py-10'>
+        <div className='flex cursor-pointer items-center space-x-3 px-4 py-3'>
+          <div className='h-12 w-12 rounded-full bg-slate-300' />
+          <div>
+            <p className='text-gry-700 text-sm'>Steve Jebs</p>
+            <p className='text-sm text-gray-500'>
+              See you tomorrow in the corner at 2pm!
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
