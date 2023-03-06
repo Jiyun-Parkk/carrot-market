8d1fb767708e859d22a98219408f3e174a3d9531
박지윤 <jyun1937@naver.com>
2023-03-06 22:32:38 +0900

    Style: client 코드 기본으로 변경

--- libs/client.ts
+++ libs/client.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const client = new PrismaClient();
-client.user.create({
-  data: {
-    name: 'jiyun',
-    email: 'jiyun@naver.com',
-  },
-});
+export default new PrismaClient();
 // npx prisma generate
 // prisma가 스키마를 확인해서 타입스크립트로 타입을 만들어준다.
 // ✔ Generated Prisma Client (4.10.1 | library) to ./node_modules/@prisma/client
